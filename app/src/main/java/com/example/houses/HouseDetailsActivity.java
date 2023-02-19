b9456e26ffd2ed1becbf9b1e48a635d0a110b72d
carolinanichita <carolinanichita89@gmail.com>
2023-02-19 16:02:19 +0100

    implemented google maps

--- app/src/main/java/com/example/houses/HouseDetailsActivity.java
+++ app/src/main/java/com/example/houses/HouseDetailsActivity.java
@@ -1,28 +1,38 @@
 package com.example.houses;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.example.houses.fragments.HomeFragment;
-
-public class HouseDetailsActivity extends AppCompatActivity implements SelectListenerInterface {
+public class HouseDetailsActivity extends AppCompatActivity {
     Button back;
     TextView priceView;
     ImageView imageView;
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_house_details);
+
+        getData();
+        setBackButton();
+        loadMap();
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+    }
+
+    private void getData() {
         priceView = findViewById(R.id.textViewHomePrice);
         imageView = findViewById(R.id.imageView15);
 
@@ -31,7 +41,27 @@ public class HouseDetailsActivity extends AppCompatActivity implements SelectLis
 
         priceView.setText(price);
         imageView.setImageResource(Integer.parseInt(image));
+    }
 
+    private void loadMap() {
+        webView = (WebView) findViewById(R.id.webViewMap);
+
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.getSettings().setLoadsImagesAutomatically(true);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
+        webView.setScrollBarStyle(View.SCROLLBARS_INSIDE_OVERLAY);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.getSettings().setPluginState(WebSettings.PluginState.ON);
+        webView.getSettings().setMediaPlaybackRequiresUserGesture(false);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
+        webView.requestFocus(View.FOCUS_DOWN);
+        webView.loadUrl("file:///android_asset/map.html");
+    }
+
+    private void setBackButton() {
         back = findViewById(R.id.backButton);
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,10 +70,5 @@ public class HouseDetailsActivity extends AppCompatActivity implements SelectLis
                 onBackPressed();
             }
         });
-    }
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
     }
 }
